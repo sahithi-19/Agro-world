8066d656779998f6eea3b4adcb11fb7703cdd963
sahithi-19 <86959607+sahithi-19@users.noreply.github.com>
2021-07-05 16:26:37 +0530

    Add files via upload

---
+++ explore.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  function showAndHidecrop1() {
+    var x = document.getElementById('crops1');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function showAndHidecrop2() {
+  var y = document.getElementById('crops2');
+  if (y.style.display == 'none') {
+      y.style.display = 'block';
+  } else {
+      y.style.display = 'none';
+  }
+}
+
+function showAndHidecrop3() {
+  var z = document.getElementById('crops3');
+  if (z.style.display == 'none') {
+      z.style.display = 'block';
+  } else {
+      z.style.display = 'none';
+  }
+}
